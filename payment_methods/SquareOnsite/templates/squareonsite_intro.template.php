1777ce3afe151c2d0d73c0f06625df3e150f57af
Nazar Kolivoshka <nazar@eventespresso.com>
2021-01-28 16:03:56 +0200

    update the intro info

--- payment_methods/SquareOnsite/templates/squareonsite_intro.template.php
+++ payment_methods/SquareOnsite/templates/squareonsite_intro.template.php
@@ -2,7 +2,7 @@
 
 printf(
     esc_html__(
-        'Square is an on-site payment method for accepting payments with credit and debit cards. Apple Pay and Google Pay are also available. This payment method is available to event organizers in the US, Canada, Japan, Australia, and the United Kingdom. An account with Square is required to accept payments. You can create one %1$son their website%2$s.',
+        'Square is an on-site payment method for accepting payments with credit and debit cards. Apple Pay and Google Pay are also available. This payment method is available to event organizers in the US, Canada, Japan, Australia, and the United Kingdom. Apple Pay is only available for Square accounts based in Canada, the United Kingdom, and the United States. An account with Square is required to accept payments. You can create one %1$son their website%2$s.',
         'event_espresso'
     ),
     '<a href="https://eventespresso.com/go/square/" target="_blank">',
