f52cfafbd8768ce1bc5384111f9a1b7fa3d6e9c9
Nazar Kolivoshka <nazar@eventespresso.com>
2021-04-16 17:05:09 +0300

    request the MERCHANT_PROFILE_READ as well

--- modules/EED_SquareOnsiteOAuth.module.php
+++ modules/EED_SquareOnsiteOAuth.module.php
@@ -196,7 +196,7 @@ class EED_SquareOnsiteOAuth extends EED_Module
             [
                 'return_url' => rawurlencode($redirectUri),
                 'scope'      => urlencode(
-                    'PAYMENTS_WRITE PAYMENTS_READ ORDERS_WRITE ORDERS_READ ITEMS_WRITE ITEMS_READ'
+                    'PAYMENTS_WRITE PAYMENTS_READ ORDERS_WRITE ORDERS_READ ITEMS_WRITE ITEMS_READ MERCHANT_PROFILE_READ'
                 ),
                 'modal'      => true
             ],
