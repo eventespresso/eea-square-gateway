c1b01626d4e1e5e0efb43e422ff691648a01e2a3
Nazar Kolivoshka <nazar@eventespresso.com>
2021-11-08 21:12:19 +0200

    Pre release cleanup (#10)
    * correct the unittest base
    
    * correct the title

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,19 +6,14 @@
 use EETests\bootstrap\AddonLoader;
 
 $core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
-//if still don't have $core_tests_dir, then let's check tmp folder.
-if (! is_dir($core_tests_dir)) {
-    $core_tests_dir = '/tmp/event-espresso-core/tests/';
-}
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 
-define('EEA_SQUARE_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
-define('EEA_SQUARE_TESTS_DIR', EEA_SQUARE_PLUGIN_DIR . 'tests/');
+$addon_path = dirname(dirname(__FILE__)) . '/';
 
 $addon_loader = new AddonLoader(
-    EEA_SQUARE_TESTS_DIR,
-    EEA_SQUARE_PLUGIN_DIR,
-    'eea-square-gateway.php'
+    $addon_path . 'tests',
+    $addon_path,
+    'eea-square-gateway.php',
 );
 $addon_loader->init();
