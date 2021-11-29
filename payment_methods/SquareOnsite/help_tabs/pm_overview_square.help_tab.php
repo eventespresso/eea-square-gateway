183761522afcfccaae3bbef39459982322e35c7f
Nazar Kolivoshka <nazar@eventespresso.com>
2021-11-29 20:43:50 +0200

    update the docs

--- payment_methods/SquareOnsite/help_tabs/pm_overview_square.help_tab.php
+++ payment_methods/SquareOnsite/help_tabs/pm_overview_square.help_tab.php
@@ -4,10 +4,10 @@
 <p>
     <?php printf(
         esc_html__(
-            'Adjust the settings for the Square payment gateway. More information can be found on %1$ssquareup.com%2$s.',
+            'Adjust the settings for the Square payment gateway. More information can be found on %1$seventespresso.com%2$s.',
             'event_espresso'
         ),
-        '<a href="https://developer.squareup.com/" target="_blank">',
+        '<a href="https://eventespresso.com/wiki/eea-square-gateway/?utm_source=github&utm_medium=link&utm_campaign=ee_addon_description_readme&utm_content=view+addon+documentation" target="_blank">',
         '</a>'
     ); ?>
 </p>
@@ -53,12 +53,35 @@
     </li>
     <li>
         <strong><?php esc_html_e('Enable Digital Wallet', 'event_espresso'); ?></strong><br />
+        <?php esc_html_e(
+            'Allows payments with digital wallets like Apple Pay, Google Pay.',
+            'event_espresso'
+        ); ?><br />
+        <?php esc_html_e('Note, to take Apple Pay payments, the following criteria must be met:', 'event_espresso'); ?>
+        <ul>
+            <li>
+                <?php esc_html_e('Apple Pay is only supported on Apple Safari browsers.', 'event_espresso'); ?>
+            </li>
+            <li>
+                <?php printf(
+                    esc_html__('Your domain has to be %1$svalidated with Apple%2$s.', 'event_espresso'),
+                    '<a href="https://developer.squareup.com/docs/web-payments/apple-pay#step-1-register-your-sandbox-domain-with-apple" target="_blank">',
+                    '</a>'
+                ); ?>
+            </li>
+            <li>
+                <?php esc_html_e(
+                    'Your website has to be on HTTPS. Apple Pay payments cannot be tested with HTTP or from localhost.',
+                    'event_espresso'
+                ); ?>
+            </li>
+        </ul>
         <?php printf(
             esc_html__(
-                'Allows payments with digital wallets like Apple Pay, Google Pay. You can read the %1$sdocumentation%2$s for more information.',
+                'You can read the %1$sdocumentation%2$s for more information.',
                 'event_espresso'
             ),
-            '<a href="https://developer.squareup.com/docs/payments-digital-wallets" target="_blank">',
+            '<a href="https://developer.squareup.com/docs/web-payments/apple-pay" target="_blank">',
             '</a>'
         ); ?>
     </li>
