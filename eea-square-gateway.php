e7a30eef591bd50a3b7a7072aa95af99f088ffad
Nazar Kolivoshka <nazar@eventespresso.com>
2021-12-14 22:46:50 +0200

    Add php version check (#23)
    * add php version check
    
    * refactor php version validation

--- eea-square-gateway.php
+++ eea-square-gateway.php
@@ -8,7 +8,7 @@
 
     Author: Event Espresso
     Author URI: https://eventespresso.com
-    Copyright 2020 Event Espresso (email : support@eventespresso.com)
+    Copyright 2021 Event Espresso (email : support@eventespresso.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
@@ -31,10 +31,41 @@ define('EEA_SQUARE_GATEWAY_PLUGIN_PATH', plugin_dir_path(__FILE__));
 // Register this add-on with EE.
 function loadEEASquareGateway()
 {
-    if (class_exists('EE_Addon')) {
-        require_once(EEA_SQUARE_GATEWAY_PLUGIN_PATH . 'EE_SquareGateway.class.php');
-        EE_SquareGateway::registerAddon();
+    if (version_compare(PHP_VERSION, '7.1', '>=')) {
+        if (class_exists('EE_Addon')) {
+            require_once(EEA_SQUARE_GATEWAY_PLUGIN_PATH . 'EE_SquareGateway.class.php');
+            EE_SquareGateway::registerAddon();
+        }
+    } else {
+        // Incompatible PHP version, disable the plugin.
+        add_action('admin_init', 'eeaSquareDisablePm');
     }
 }
 add_action('AHEE__EE_System__load_espresso_addons', 'loadEEASquareGateway');
+
+// Check PHP version etc.
+function eeaSquareDisablePm()
+{
+    unset($_GET['activate'], $_REQUEST['activate']);
+    if (! function_exists('deactivate_plugins')) {
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    }
+    deactivate_plugins(plugin_basename(EEA_SQUARE_GATEWAY_PLUGIN_FILE));
+    add_action('admin_notices', 'eeaSquareDisablePmNotice');
+}
+
+// The deactivation notice.
+function eeaSquareDisablePmNotice()
+{
+    echo '<div class="error"><p>'
+         . sprintf(
+             esc_html__(
+                 '%1$s Event Espresso - Square Gateway %2$s was deactivated! This plugin requires a %1$sPHP version 7.1 or higher%2$s to work properly.',
+                 'event_espresso'
+             ),
+             '<strong>',
+             '</strong>'
+         )
+         . '</p></div>';
+}
 // End of file eea-square-gateway.php
