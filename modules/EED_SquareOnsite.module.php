96665cc29dcec6265f77cfd5a6dc21c5fbd77c10
Nazar Kolivoshka <nazar@eventespresso.com>
2022-01-03 16:25:58 +0200

    Populate billing info for payments (#15)
    * populate billing info for payments
    
    * a different method name was used
    
    * bump the min core version

--- modules/EED_SquareOnsite.module.php
+++ modules/EED_SquareOnsite.module.php
@@ -112,4 +112,21 @@ class EED_SquareOnsite extends EED_Module
             $CancelOrder->cancel($orderId, $orderVersion);
         }
     }
+
+
+    /**
+     * Gets the country ISO by its name
+     *
+     * @param string $country_name
+     * @return string
+     * @throws EE_Error|ReflectionException
+     */
+    public static function getCountryIsoByName(string $country_name): string
+    {
+        $country_model = EEM_Country::instance();
+        $country = method_exists($country_model, 'getCountryByName')
+            ? $country_model->getCountryByName($country_name)
+            : $country_model->get_one([['CNT_name' => $country_name]]);
+        return $country instanceof EE_Country ? $country->ID() : '';
+    }
 }
